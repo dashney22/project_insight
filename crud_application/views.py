d78f0bfb101cf06be0cbb9be4b8178f034f01946
dashney22 <dashneymakgafela@gmail.com>
2024-06-26 11:52:05 +0200

    Added towns functionality

--- crud_application/views.py
+++ crud_application/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Town
 
 # Create your views here.
 
+# View for veiwing all towns in the database {using DBSQLITE}
 def home_view(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        new_town_name = request.POST.get('new_town_name')
+        if new_town_name:
+            Town.objects.create(name=new_town_name)  
+            return redirect('home')  # Redirect to refresh town list
+    
+    towns = Town.objects.all()
+
+    context = {
+        'towns': towns,
+    }
+    return render(request, 'home.html', context)
+
